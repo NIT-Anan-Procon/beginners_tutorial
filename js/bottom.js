427fd52acb446327efa9ae5033836ee2023566e8
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-03-06 10:23:21 +0900

    [update]スマホで見たときに「前に戻る、次へ進む」などのテキストを簡略化

--- js/bottom.js
+++ js/bottom.js
@@ -1,43 +1,62 @@
 file_name_first = Number(file_name_first);
-if (file_name_first != 0 && file_name_first != "NaN")
-  if (file_name_first == 1 && file_name_last == 1)
-    in_html = "<a href='index.html' class=bottom_left>← 前に戻る</a>";
-  else if (file_name_last == 1)
-    in_html =
-      "<a href='" +
-      ("00" + (file_name_first - 1)).slice(-2) +
-      ("00" + (big_middle_header_box[file_name_first - 1].length - 1)).slice(
-        -2
-      ) +
-      ".html' class=bottom_left>← 前に戻る</a>";
-  else
-    in_html =
-      "<a href='" +
-      ("00" + file_name_first).slice(-2) +
-      "-" +
-      ("00" + (file_name_last - 1)).slice(-2) +
-      ".html' class=bottom_left>← 前に戻る</a>";
-else in_html = "<a class=bottom_left></a>";
-in_html += "<a href='#header' class=bottom_center>ページトップへ</a>";
-if (
-  file_name_first != "NaN" &&
-  !(
-    big_middle_header_box[file_name_first].length - 1 == file_name_last &&
-    big_middle_header_box[file_name_first + 1].length <= 1
+let $win = $(window);
+$win.on("load resize", function () {
+  let windowWidth = window.innerWidth;
+  if (windowWidth > 1000) {
+    bottom_txt("← 前に戻る", "ページトップへ", "次へ進む →");
+  } else {
+    bottom_txt("← 前へ", "トップへ", "次へ →");
+  }
+});
+function bottom_txt(left_txt, center_txt, right_txt) {
+  if (file_name_first != 0 && file_name_first != "NaN")
+    if (file_name_first == 1 && file_name_last == 1)
+      in_html = "<a href='index.html' class=bottom_left>" + left_txt + "</a>";
+    else if (file_name_last == 1)
+      in_html =
+        "<a href='" +
+        ("00" + (file_name_first - 1)).slice(-2) +
+        ("00" + (big_middle_header_box[file_name_first - 1].length - 1)).slice(
+          -2
+        ) +
+        ".html' class=bottom_left>" +
+        left_txt +
+        "</a>";
+    else
+      in_html =
+        "<a href='" +
+        ("00" + file_name_first).slice(-2) +
+        "-" +
+        ("00" + (file_name_last - 1)).slice(-2) +
+        ".html' class=bottom_left>" +
+        left_txt +
+        "</a>";
+  else in_html = "<a class=bottom_left></a>";
+  in_html += "<a href='#header' class=bottom_center>" + center_txt + "</a>";
+  if (
+    file_name_first != "NaN" &&
+    !(
+      big_middle_header_box[file_name_first].length - 1 == file_name_last &&
+      big_middle_header_box[file_name_first + 1].length <= 1
+    )
   )
-)
-  if (big_middle_header_box[file_name_first].length - 1 == file_name_last)
-    in_html +=
-      "<a href='" +
-      ("00" + (file_name_first + 1)).slice(-2) +
-      "-01.html' class=bottom_right>次へ進む →</a>";
-  else
-    in_html +=
-      "<a href='" +
-      ("00" + file_name_first).slice(-2) +
-      "-" +
-      ("00" + (file_name_last + 1)).slice(-2) +
-      ".html' class=bottom_right>次へ進む →</a>";
-else in_html += "<a class=bottom_right></a>";
+    if (big_middle_header_box[file_name_first].length - 1 == file_name_last)
+      in_html +=
+        "<a href='" +
+        ("00" + (file_name_first + 1)).slice(-2) +
+        "-01.html' class=bottom_right>" +
+        right_txt +
+        "</a>";
+    else
+      in_html +=
+        "<a href='" +
+        ("00" + file_name_first).slice(-2) +
+        "-" +
+        ("00" + (file_name_last + 1)).slice(-2) +
+        ".html' class=bottom_right>" +
+        right_txt +
+        "</a>";
+  else in_html += "<a class=bottom_right></a>";
 
-bottom.innerHTML = in_html;
+  bottom.innerHTML = in_html;
+}
