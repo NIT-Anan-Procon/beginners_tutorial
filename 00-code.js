266b53042a3686df29a049d933d057932c9321de
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-03-09 22:53:02 +0900

    :hammer:smallheaderを自動で挿入されるように変更

--- 00-code.js
+++ 00-code.js
@@ -56,5 +56,6 @@ $(".code").each(function () {
   in_html += ".replace(/m@/g,'<span style=\"color:#f0e68c\">')";
   in_html += ".replace(/@m/g,'</span>')";
   in_html += ");";
+
   eval(in_html);
 });
