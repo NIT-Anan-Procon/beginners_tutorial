4d8342e60c645c203a5593cf18477f9acfccc99b
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-03-10 00:37:14 +0900

    :ambulance:番外編2つに小見出しとページジャンプが表示されない不具合の修正

--- js/bottom.js
+++ js/bottom.js
@@ -1,4 +1,5 @@
-file_name_first = Number(file_name_first);
+if (file_name_first !== "en" && file_name_first !== "an")
+  file_name_first = Number(file_name_first);
 let $win = $(window);
 $win.on("load resize", function () {
   let windowWidth = window.innerWidth;
@@ -12,7 +13,11 @@ $win.on("load resize", function () {
 });
 
 function bottom_txt(left_txt, center_txt, right_txt) {
-  if (file_name_first != 0 && file_name_first != "NaN")
+  if (
+    file_name_first != 0 &&
+    file_name_first !== "en" &&
+    file_name_first !== "an"
+  )
     if (file_name_first == 1 && file_name_last == 1)
       in_html = "<a href='index.html' class=bottom_left>" + left_txt + "</a>";
     else if (file_name_last == 1)
@@ -37,7 +42,8 @@ function bottom_txt(left_txt, center_txt, right_txt) {
   else in_html = "<a class=bottom_left></a>";
   in_html += "<a href='#header' class=bottom_center>" + center_txt + "</a>";
   if (
-    file_name_first != "NaN" &&
+    file_name_first !== "en" &&
+    file_name_first !== "an" &&
     !(
       big_middle_header_box[file_name_first].length - 1 == file_name_last &&
       big_middle_header_box[file_name_first + 1].length <= 1
--- js/small_header.js
+++ js/small_header.js
@@ -1,31 +1,38 @@
 let txt = article.outerHTML;
-for (
-  let i = 0;
-  i < small_header_box[file_name_first][file_name_last - 1].length;
-  i++
-) {
-  in_html = "<section class=section><h3 id=P";
-  if (file_name_first === "en") setSubSmallHeader(0);
-  else if (file_name_first === "an") setSubSmallHeader(1);
-  else
+console.log(file_name_first);
+if (file_name_first === "en") setSubSmallHeader(0);
+else if (file_name_first === "an") setSubSmallHeader(1);
+else
+  for (
+    let i = 0;
+    i < small_header_box[file_name_first][file_name_last - 1].length;
+    i++
+  ) {
+    in_html = "<section class=section><h3 id=P";
     in_html +=
       small_header_counter +
       ">" +
       small_header_box[file_name_first][file_name_last - 1][
         small_header_counter - 1
       ];
+    in_html += "</h3>";
 
-  in_html += "</h3>";
+    small_header_counter++;
+    txt = txt.replace("<section>", in_html);
+  }
 
-  function setSubSmallHeader(file_name_first_cp) {
+function setSubSmallHeader(file_name_first_cp) {
+  for (let i = 0; i < sub_content_header_box[file_name_first_cp].length; i++) {
+    in_html = "<section class=section><h3 id=P";
     in_html +=
       small_header_counter +
       ">" +
       sub_content_header_box[file_name_first_cp][small_header_counter + 1];
-  }
+    in_html += "</h3>";
 
-  small_header_counter++;
-  txt = txt.replace("<section>", in_html);
+    small_header_counter++;
+    txt = txt.replace("<section>", in_html);
+  }
 }
 
 article.innerHTML = txt;
